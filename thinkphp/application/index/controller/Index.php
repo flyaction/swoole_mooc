aca7ba354b587ac125d6f8045c28eda9d2ea4777
xingdong <xingdong1117@126.com>
2018-05-17 16:38:45 +0800

    发送短信

--- thinkphp/application/index/controller/Index.php
+++ thinkphp/application/index/controller/Index.php
@@ -1,6 +1,9 @@
 <?php
 namespace app\index\controller;
 
+use app\common\lib\ali\Sms;
+use think\Exception;
+
 class Index
 {
     public function index()
@@ -16,5 +19,22 @@ class Index
     public function hello($name = 'ThinkPHP5')
     {
         return 'hello,' . $name;
+    }
+
+    public function sms(){
+
+        try
+        {
+            $res = Sms::sendSms('15010115181','123');
+            return $res;
+        }
+        catch (Exception $e)
+        {
+
+        }
+
+
+
+
     }
 }
---
+++ thinkphp/application/index/controller/Send.php
@@ -0,0 +1,18 @@
+<?php
+namespace app\index\controller;
+
+use app\common\lib\ali\Sms;
+use think\Exception;
+
+class Send
+{
+    /*
+     * 发送验证码
+     */
+    public function index()
+    {
+
+        echo 1;
+    }
+
+}
